a8e6d9be24d93f6ebd9f1950d0804225488c0311
Raden Dhaneswara <bennyscar23@gmail.com>
2022-10-06 14:37:14 +0700

    Finish lab 5

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from wishlist.views import create_list
+from wishlist.views import show_wishlist_ajax
 from wishlist.views import show_wishlist
 from wishlist.views import show_xml 
 from wishlist.views import show_json
@@ -17,6 +19,9 @@ urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
+    path('ajax/', show_wishlist_ajax, name='show_wishlist_ajax'),
+    path('ajax/submit', create_list, name='create_list')
+
     # path('', include('example_app.urls')),
     # path('wishlist/', include('wishlist.urls')),
 ]
--- wishlist/views.py
+++ wishlist/views.py
@@ -11,7 +11,7 @@ from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-
+from django.views.decorators.csrf import csrf_exempt
 
 @login_required(login_url='/wishlist/login/')
 def show_wishlist(request):
@@ -22,6 +22,16 @@ def show_wishlist(request):
     'last_login': request.COOKIES['last_login'],
     }
     return render(request, "wishlist.html", context)
+
+@login_required(login_url='login/')
+def show_wishlist_ajax(request):
+    data_barang_wishlist = BarangWishlist.objects.all()
+    context = {
+    'list_barang': data_barang_wishlist,
+    'nama': 'Raden',
+    'last_login': request.COOKIES['last_login'],
+    }
+    return render(request, "wishlist_ajax.html", context)
     
 def show_xml(request):
     data = BarangWishlist.objects.all()
@@ -70,5 +80,16 @@ def logout_user(request):
     response.delete_cookie('last_login')
     return response
 
+@login_required(login_url='login/')
+@csrf_exempt #referensi: https://docs.djangoproject.com/en/4.1/ref/csrf/
+def create_list(request):
+    if request.method == "POST":
+        nama_barang = request.POST.get('nama_barang')
+        harga_barang = request.POST.get('harga_barang')
+        deskripsi = request.POST.get('deskripsi')
+        create_wishlist = BarangWishlist.objects.create(nama_barang=nama_barang,harga_barang=harga_barang,deskripsi=deskripsi)
+        systemresponse = HttpResponseRedirect(reverse("create_wishlist:show_wishlist_ajax"))
+
+        return systemresponse
     
 # Create your views here.
